a51ed8c1d232fdac0da1f3a315812858583a381d
stekl0 <greystekl0@gmail.com>
2025-03-30 20:03:04 +0500

    Ensure decimal can only be added once per number segment

--- app/src/main/java/com/github/greysteklo/anotherone/calculator/domain/usecase/EnterDecimalUseCase.kt
+++ app/src/main/java/com/github/greysteklo/anotherone/calculator/domain/usecase/EnterDecimalUseCase.kt
@@ -5,5 +5,26 @@ import javax.inject.Inject
 class EnterDecimalUseCase
     @Inject
     constructor() {
-        fun execute(expression: String): String = if (expression.last() == ',') expression else "$expression,"
+        fun execute(expression: String): String {
+            val lastChar = expression.last()
+            if (lastChar == ',' || lastChar in "+-*/") {
+                return expression
+            }
+
+            val lastOperatorIndex =
+                expression.indexOfLast { it in "+-*/" }
+
+            val lastNumberSegment =
+                if (lastOperatorIndex == -1) {
+                    expression
+                } else {
+                    expression.substring(lastOperatorIndex + 1)
+                }
+
+            return if (lastNumberSegment.contains(',')) {
+                expression
+            } else {
+                "$expression,"
+            }
+        }
     }
