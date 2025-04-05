c5930837ad74d8911c46255cf1d85290e8305848
stekl0 <greystekl0@gmail.com>
2025-04-05 12:23:36 +0500

    Change minimum SDK version from 29 to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,7 +12,7 @@ android {
 
     defaultConfig {
         applicationId = "com.github.greysteklo.anotherone.calculator"
-        minSdk = 29
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
         versionName = "1.0"
