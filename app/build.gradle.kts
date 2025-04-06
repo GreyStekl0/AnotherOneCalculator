9b13065c44d031eaaa898e3a832fdeff0a489daa
stekl0 <greystekl0@gmail.com>
2025-04-06 22:46:12 +0500

    Update app version code to 2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         applicationId = "com.github.greysteklo.anotherone.calculator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
