a71a5cab4bbed5bd75f61b8aef7a2c0378ef889e
stekl0 <greystekl0@gmail.com>
2025-05-31 13:56:52 +0500

    bump version to 1.4.7

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.github.greysteklo.anotherone.calculator"
         minSdk = 26
         targetSdk = 35
-        versionCode = 12
-        versionName = "1.4.6"
+        versionCode = 13
+        versionName = "1.4.7"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
