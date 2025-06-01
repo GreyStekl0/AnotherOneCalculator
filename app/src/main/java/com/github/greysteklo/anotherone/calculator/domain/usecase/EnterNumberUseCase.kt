4703851239e474ec63a524b191dab8cd942bd0ce
stekl0 <greystekl0@gmail.com>
2025-06-01 21:45:07 +0500

    Improve EnterNumberUseCase to handle decimal input

--- app/src/main/java/com/github/greysteklo/anotherone/calculator/domain/usecase/EnterNumberUseCase.kt
+++ app/src/main/java/com/github/greysteklo/anotherone/calculator/domain/usecase/EnterNumberUseCase.kt
@@ -7,7 +7,10 @@ import javax.inject.Inject
 class EnterNumberUseCase
     @Inject
     constructor() {
-        val numberFormat: NumberFormat = NumberFormat.getNumberInstance(Locale("ru", "RU"))
+        val numberFormat: NumberFormat =
+            NumberFormat
+                .getNumberInstance(Locale("ru", "RU"))
+                .apply { maximumFractionDigits = Int.MAX_VALUE }
 
         fun execute(
             expression: String,
@@ -19,7 +22,11 @@ class EnterNumberUseCase
                 val lastNumber = getLastNumber(expression)
                 if (lastNumber != null) {
                     val prefix = expression.dropLast(lastNumber.length)
-                    val newLastNumber = (lastNumber + number).replace(Regex("\\s+"), "").toBigDecimal()
+                    val newLastNumber =
+                        (lastNumber + number)
+                            .replace(Regex("\\s+"), "")
+                            .replace(",", ".")
+                            .toBigDecimal()
                     prefix + numberFormat.format(newLastNumber)
                 } else {
                     expression + number.toString()
@@ -28,7 +35,7 @@ class EnterNumberUseCase
     }
 
 private fun getLastNumber(expression: String): String? {
-    val regex = """(\d{1,3}(?:\s\d{3})*)$""".toRegex()
+    val regex = """(\d{1,3}(?:\s\d{3})*(?:,\d+)?)$""".toRegex()
     val matchResult = regex.find(expression)
     return matchResult?.groupValues?.get(1)
 }
