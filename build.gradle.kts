208643cbb49b053de8ddd87fa0496841764e0642
stekl0 <greystekl0@gmail.com>
2025-06-18 13:46:30 +0500

    Update Gradle dependencies and plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,6 @@
+import com.deezer.caupain.model.StabilityLevelPolicy
+import com.deezer.caupain.plugin.DependenciesUpdateTask
+
 // Top-level build file where you can add configuration options common to all sub-projects/modules.
 plugins {
     alias(libs.plugins.android.application) apply false
@@ -6,4 +9,11 @@ plugins {
     alias(libs.plugins.android.ksp) apply false
     alias(libs.plugins.android.hilt) apply false
     alias(libs.plugins.android.room) apply false
+    alias(libs.plugins.caupain) apply true
+}
+
+caupain {
+    tasks.withType<DependenciesUpdateTask> {
+        selectIf(StabilityLevelPolicy)
+    }
 }
