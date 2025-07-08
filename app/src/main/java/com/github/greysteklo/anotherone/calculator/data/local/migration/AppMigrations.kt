3079cc317075c392f2b99f8af0e7f8b31afe4aa5
stekl0 <greystekl0@gmail.com>
2025-07-08 15:51:02 +0300

    Refactor migration function to use consistent parameter naming

--- app/src/main/java/com/github/greysteklo/anotherone/calculator/data/local/migration/AppMigrations.kt
+++ app/src/main/java/com/github/greysteklo/anotherone/calculator/data/local/migration/AppMigrations.kt
@@ -5,8 +5,8 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 
 val MIGRATION_1_2 =
     object : Migration(1, 2) {
-        override fun migrate(database: SupportSQLiteDatabase) {
-            database.execSQL("ALTER TABLE calculation_history ADD COLUMN date INTEGER NOT NULL DEFAULT 0")
+        override fun migrate(db: SupportSQLiteDatabase) {
+            db.execSQL("ALTER TABLE calculation_history ADD COLUMN date INTEGER NOT NULL DEFAULT 0")
         }
     }
 
